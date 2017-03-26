1a92566ddc31772ad8f7a98ee06f6983564993a3
Konrad M. Kowalczyk <kamkan17@gmail.com>
2017-03-26 21:09:19 +0000

    xd

--- kmk/projekt.java
+++ kmk/projekt.java
@@ -6,7 +6,7 @@ public class projekt{
     throws InterruptedException{
         Scanner in = new Scanner(System.in);
         projekt funkcje = new projekt();
-        
+        int karma = 0;
         String imie;
     System.out.println("Witaj, jak masz na imię? ");
     imie = in.nextLine();
@@ -51,14 +51,33 @@ public class projekt{
         }
         
         System.out.println("*rozmazana rzeczywistość*");
-        Thread.sleep(1200);
+        Thread.sleep(1600);
         System.out.println("Nieznajomy: " + imie + " tak? Jestem oczami śmierci i głosem bezkresu.");
-        Thread.sleep(1200);
+        Thread.sleep(5000);
         System.out.println(imie + ": Jaa... Czy tu jesteś? Czy ja śnię?");
-        
-        
-
-        
+        Thread.sleep(2300);        
+        System.out.println("Nieznajomy: I Ty tu jesteś, i ja, a teraz powiedz mi proszę pare rzeczy o sobie.");
+        Thread.sleep(3800);
+        System.out.println("{1: Ok, co chcesz wiedzieć?");
+        System.out.println("{2: Chwila, gdzie jesteśmy? Co tu robisz?");
+        System.out.println("{3: Spadaj zjawo... chcę się obudzić.");
+        int answ1 = in.nextInt();
+        switch(answ1){
+            case 1:
+                System.out.println("Nieznajomy: Nie boisz się mnie, super. Powiedz mi ");
+                break;
+            case 2:
+                System.out.println("Nieznajomy: Jesteśmy w krainie snów... Jestem tu żeby przekazać Ci wiadomość...");
+                break;
+            default:
+                    System.out.println("*sen*");
+        for(int sen = 0; sen <3; sen++){
+            System.out.println("zzz...");
+            Thread.sleep(1200);
+            System.out.println("");
+            Thread.sleep(1200);
+        }
+        }
     }
     else
     System.exit(0);
